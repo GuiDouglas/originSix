9b5102d35d36846ffdf2039d636981a2ac376323
GuiDouglas <86529183+GuiDouglas@users.noreply.github.com>
2022-07-20 20:17:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -45,24 +45,6 @@ const swiper = new Swiper('.swiper', {
     }
   });
 
-// scrollReview
-
-// const scrollReveal = ScrollReveal({
-//     origin: 'top', 
-//     distance: '30px',
-//     duration: 500,
-//     reset: true
-// })
-
-// scrollReveal.reveal(
-//     `#home .text, #home .image,
-//     #about .image, #about .text,
-//     #services header, #services .card,
-//     #testimonials header, #testimonials .testimonials
-//     #contact .text, #contact .links,
-//     footer .brand, footer .social
-// `, {interval: 100})
-
 // back to top
 
 function backToTop(){
